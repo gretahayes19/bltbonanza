9d62be82d5dabc0ae5b77814b693b7ccddd1391d
Greta <gretahayes19@gmail.com>
2021-04-07 14:08:48 -0400

    stacking works! and some css

--- src/index.js
+++ src/index.js
@@ -17,29 +17,13 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
 
+
 let score = 0;
 let gameFrame = 0;
-ctx.font = "50px Georgia";
-
-//Mouse Stuff
+ctx.font = "30px Bungee Shade";
 
 let canvasPos = canvas.getBoundingClientRect();
 console.log(canvasPos);
-
-const mouse = {
-  x: canvas.width / 2,
-  y: canvas.height / 2,
-  click: false
-}
-
-
-
-canvas.addEventListener('mousedown', function (event) {
-  mouse.click = true;
-  mouse.x = event.x - canvasPos.left;
-  mouse.y = event.y - canvasPos.top;
-  // console.log(mouse.x, mouse.y)
-});
 
 const keys = {
   leftPressed: false,
@@ -51,14 +35,8 @@ document.addEventListener('keydown', function (e) {
     case 37:
       keys.leftPressed = true;
       break;
-    case 38:
-      alert('up');
-      break;
     case 39:
       keys.rightPressed = true;
-      break;
-    case 40:
-      alert('down');
       break;
   }
 });
@@ -68,22 +46,16 @@ document.addEventListener('keyup', function (e) {
     case 37:
       keys.leftPressed = false;
       break;
-    case 38:
-      alert('up');
-      break;
     case 39:
       keys.rightPressed = false;
-      break;
-    case 40:
-      alert('down');
       break;
   }
 });
 
-canvas.addEventListener('mouseup', function (event) {
-    mouse.click = false;
-    console.log(mouse.x, mouse.y)
-});
+// canvas.addEventListener('mouseup', function (event) {
+//     mouse.click = false;
+//     console.log(mouse.x, mouse.y)
+// });
 
 
 //food
@@ -93,7 +65,13 @@ const lettuce = new Image();
 lettuce.src = './dist/images/lettuce.png'
 const bread = new Image();
 bread.src = './dist/images/bread.png'
-const ingredients = [tomato, lettuce]
+const bacon = new Image();
+bacon.src = './dist/images/bacon.png'
+const mayo = new Image();
+mayo.src = './dist/images/mayo.png'
+const mayoflat = new Image();
+mayoflat.src = './dist/images/mayoflat.png'
+const ingredients = [tomato, mayo, lettuce, bacon, tomato, mayo, lettuce, bacon, bread]
 
 
 const foodArr = [];
@@ -133,18 +111,22 @@ class Bread{
     if (keys.leftPressed && (this.x !== 0)) {
       this.x -= 5;
     }
-    if (keys.rightPressed && (this.x !== canvas.width)) {
+    if (keys.rightPressed && (this.x !== canvas.width-130)) {
       this.x += 5;
     }
   }
 
   draw(contacted) {
     let stackx = this.x;
-    let stacky = this.y - 20;
-    ctx.drawImage(bread, 0, 0, 1000, 1000, this.x, this.y, 300, 300);
+    let stacky = canvas.height - 100;
+    ctx.drawImage(bread, 0, 0, 1000, 1000, this.x, canvas.height-100, 300, 300);
     if (contacted) {
       for (let i = 0; i < contacted.length; i++) {
-        ctx.drawImage(contacted[i].ingredient, 0, 0, 1000, 1000, stackx, stacky, 250, 250);
+        if (contacted[i].ingredient) {
+          ctx.drawImage(contacted[i].ingredient, 0, 0, 1000, 1000, stackx, stacky, 250, 250);
+        } else {
+          ctx.drawImage(contacted[i], 0, 0, 1000, 1000, stackx, stacky, 250, 250);
+        }
         stacky = stacky - 20;
       }
     }
@@ -160,17 +142,39 @@ function handleFood() {
   }
   for (let i = 0; i < foodArr.length; i++) {
     if ((foodArr[i].y === thisbread.y) && (foodArr[i].x > thisbread.x - 50 && foodArr[i].x < thisbread.x + 50)) {
+
       if (!foodArr[i].counted) {
         score += 1;
       }
-      contacted.push(foodArr[i]);
-      foodArr[i].counted = true;
+      if (contacted.indexOf(foodArr[i]) === -1 ) {
+        debugger
+        if (foodArr[i].ingredient.currentSrc.includes("mayo")) {
+          contacted.push(mayoflat)
+          foodArr[i].counted = true;
+          foodArr.shift(foodArr[i])
+        } else {
+          foodArr[i].counted = true;
+
+          contacted.push(foodArr[i]);
+        }
+        thisbread.y -= 20;
+      }
+      
+      // foodArr[i].counted = true;
       break;
     }
     thisbread.draw(contacted);
 
     foodArr[i].update();
-    foodArr[i].draw();
+    if (contacted.indexOf(foodArr[i]) === -1 && foodArr[i].y < canvas.height) {
+      foodArr[i].draw();
+    }  
+
+    if (foodArr[i].y > canvas.height) {
+      foodArr.shift(foodArr[i])
+    }  
+
+    
 
   }
 
@@ -182,9 +186,8 @@ function animate() {
   thisbread.draw(contacted);
   thisbread.update();
   handleFood();
-  // food.update();
   gameFrame++;
-  ctx.fillStyle = "black";
+  ctx.fillStyle = "white";
   ctx.fillText("score:" + score, 10, 50)
   requestAnimationFrame(animate);
 }
